c3589382fa8f84fbeb6011a89c3d9e149f7e0fb9
santi <robincraft@outlook.co.th>
2026-02-13 04:18:53 +0700

    BubbleSort

---
+++ BubbleSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void bubblesort(vector<int>&arr){
+    for(int i = 0 ; i < arr.size() ; i++){
+        for(int j = 0 ;  j < arr.size() - i - 1  ;  j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+void print(vector<int>&arr){
+    for(int i = 0 ; i < arr.size() ; i++){
+        cout << arr[i] << " ";
+    }
+}
+int main(){
+    vector<int> arr = {8,83,10,21,25,41,52,46,49,17,30,58,33,4,86};
+    bubblesort(arr);
+    print(arr);
+}
